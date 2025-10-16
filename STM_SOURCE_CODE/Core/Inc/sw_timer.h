409eeb9bbd2af09447a0f835169b5cc93cdadb69
PAH23\anh_hungg23 <hung.phamdautinchi1@hcmut.edu.vn>
2025-10-16 13:01:17 +0700

    commit main

---
+++ STM_SOURCE_CODE/Core/Inc/sw_timer.h
@@ -0,0 +1,17 @@
+/*
+ * sw_timer.h
+ *
+ *  Created on: Oct 16, 2025
+ *      Author: anh_hungg23
+ */
+
+#ifndef INC_SW_TIMER_H_
+#define INC_SW_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+
+#endif /* INC_SW_TIMER_H_ */
---
+++ STM_SOURCE_CODE/Core/Src/sw_timer.c
@@ -0,0 +1,26 @@
+/*
+ * sw_timer.c
+ *
+ *  Created on: Oct 16, 2025
+ *      Author: anh_hungg23
+ */
+
+
+#include "sw_timer.h"
+
+int timer1_counter =0;
+int timer1_flag = 0;
+
+void setTimer1 (int duration) {
+	timer1_counter =duration;
+	timer1_flag =0;
+}
+
+void timerRun() {
+	if(timer1_counter > 0) {
+		timer1_counter--;
+		if(timer1_counter <=0) {
+			timer1_flag = 1;
+		}
+	}
+}
